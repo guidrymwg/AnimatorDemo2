850d79e35393e8d93bb027124d5a3635e535f2e9
guidrymwg <guidry@utk.edu>
2016-07-07 09:35:55 -0400

    All code added

--- app/src/main/java/com/lightcone/animatordemo2/MainActivity.java
+++ app/src/main/java/com/lightcone/animatordemo2/MainActivity.java
@@ -2,12 +2,39 @@ package com.lightcone.animatordemo2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    MotionRunner2 mrunner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        startAnimation();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Stop animation if going into background
+        mrunner.stopLooper();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Resume animation
+        startAnimation();
+    }
+
+    // Method to start the animation
+
+    private void startAnimation(){
+        // Instantiate the class MotionRunner to define the entry screen display
+        mrunner = new MotionRunner2(this);
+        mrunner.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        setContentView(mrunner);
     }
 }
